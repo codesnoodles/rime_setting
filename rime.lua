564077a2d96b6d2b6bea5c89bdea5623b8f07d9b
codesnoodles <zhangsworld@163.com>
2023-12-21 22:21:06 +0800

    a repo for rime setting

---
+++ rime.lua
@@ -0,0 +1,21 @@
+function date_translator(input, seg)
+	if (input == "orq") then
+		--- Candidate(type, start, end, text, comment)
+		yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
+		yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), " "))
+		yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), " "))
+		yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), " "))
+	end
+end
+
+function time_translator(input, seg)
+	if (input == "ouj") then
+		local cand = Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), " ")
+		cand.quality = 1
+		yield(cand)
+	end
+end
+
+calculator_translator = require("calculator_translator")
+vim_mode = require("vim_mode")
+number = require("number")
